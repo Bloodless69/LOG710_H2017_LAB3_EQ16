a4634e3ffbd25c52ad3cef3c2bb27093dd1ae0a3
Rouquin <AM05280@ens.etsmtl.ca>
2017-03-21 19:23:22 -0400

    Bloc and node structures

--- main.c
+++ main.c
@@ -3,8 +3,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+typedef struct bloc {
+	int size;
+	int offset;
+	void *data;
+} bloc;
+
+typedef struct node {
+	struct bloc *value;
+	struct node *previous;
+	struct node *next;
+} node;
+
 
 void main() {
-	
+	node *root = malloc(sizeof(node));
 }
 
