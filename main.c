d3e82b0b593b8c7429a44bee5739818c4c90e6af
Rouquin <AM05280@ens.etsmtl.ca>
2017-03-21 20:09:15 -0400

    Started implentation of liberemem()

--- main.c
+++ main.c
@@ -29,6 +29,46 @@ void initmem(int memSize) {
 	root->next = NULL;
 }
 
+boolean hasNext(node* node) {
+	return (node->next != NULL);
+}
+
+void liberemem(bloc* bloc) {
+	node* currentNode = root;
+
+	// If we are trying to free the root, 
+	// we need to check the next node only
+	if (currentNode->value == bloc)
+	{
+		// Checking next node
+		if (hasNext(currentNode)) {
+			// Root offset
+			currentOffset = currentNode->value->offset;
+			// Next node offset
+			nextOffset = currentNode->next->value->offset;
+		} else {
+			currentNode->value->data = NULL;
+		}
+	// We are tying to free another node in the linked list	
+	} else {
+		while (hasNext(currentNode)) {
+			// Switching to next node
+			currentNode = currentNode->next;
+		}
+	}
+}
+
+void liberemem2(bloc* bloc) {
+	node* currentNode = root;
+
+	while (hasNext(currentNode)) {
+			// Switching to next node
+			currentNode = currentNode->next;
+
+
+	}
+}
+
 void main() {
 	initmem(500);
 }
