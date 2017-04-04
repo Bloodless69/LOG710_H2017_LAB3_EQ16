2d4e0f3ebed4f43df43a503c6ed440384d266f0b
Rouquin <AM05280@ens.etsmtl.ca>
2017-04-04 15:18:45 -0400

    Refixed merged conflicts

--- manageMemoryPart1.c
+++ manageMemoryPart1.c
@@ -81,11 +81,7 @@ bloc* allocateMemory(int blocSize) {
             }
         } else {
             if(currentNode->next == NULL) {
-<<<<<<< HEAD:manageMemoryPart1.c
                 return NULL;
-=======
-                break;
->>>>>>> a5c1bf38455b9cab9622036f7eab1132e830025c:main.c
             }
             currentNode = currentNode->next;
             currentBloc = currentNode->value;
@@ -154,12 +150,8 @@ bool freeMemory(bloc* bloc) {
 				if(rightNode != NULL) {
 					rightNode->previous = newNode;
 				}
-<<<<<<< HEAD:manageMemoryPart1.c
 				if(node->previous == root)
 					root = newNode;
-=======
-				root = newNode;
->>>>>>> a5c1bf38455b9cab9622036f7eab1132e830025c:main.c
 			// If next node only has no data,
 			// we want to merge current node with the next one
 			} else if(exist(node->next) && !hasData(node->next)) {
@@ -194,11 +186,7 @@ bool freeMemory(bloc* bloc) {
 			if(node->next) {
 				node = node->next;
 			} else {
-<<<<<<< HEAD:manageMemoryPart1.c
 				return false;
-=======
-				printf("Wasn't able to free the memory. Could not find block of memory!\n");
->>>>>>> a5c1bf38455b9cab9622036f7eab1132e830025c:main.c
 			}
 			
 		}
@@ -283,54 +271,35 @@ bool byteIsAllocated(int byte) {
 			currentNode = currentNode->next;
 		}
 	}
-<<<<<<< HEAD:manageMemoryPart1.c
-=======
-	printf("Byte is not allocated\n");
->>>>>>> a5c1bf38455b9cab9622036f7eab1132e830025c:main.c
 	return false;
 }
 
-node* findAvailableNodeByBlocSize(int blocSize){
-	node *currentNode = root;
-	while(currentNode != NULL) {
-		if (!hasData(currentNode)) {
-			bloc *currentBloc = currentNode->value;
-			if (currentBloc->size == blocSize)
-				return currentNode;
-		}
-		currentNode = currentNode->next;
-	}
-	return NULL;
-}
-
-// Affiche_etat_memoire
-void printMemoryState() {
-	printf("MEMORY STATE\n");
+void printContent() {
 	int nodeIndex = 0;
 	node *currentNode = root;
 	while(currentNode != NULL) {
+		printf("current adress = %p\n", currentNode);
 		printf("Current node : %d\n", nodeIndex);
-		printf("Address = %p\n", currentNode);
 		printf("Size : %d\n", currentNode->value->size);
 		printf("Offset : %d\n", currentNode->value->offset);
 		printf("HasData : %d\n", currentNode->value->data != NULL);
 
 		if(currentNode->previous != NULL) {
-			printf("Address of previous node = %p\n", currentNode->previous);
-			printf("Next address of previous node = %p\n", currentNode->previous->next);
+			printf("current adress previous = %p\n", currentNode->previous);
+			printf("current adress previous next = %p\n", currentNode->previous->next);
 		}
 		if(currentNode->next != NULL) {
-			printf("Address of next node = %p\n", currentNode->next);
-			printf("Previous address of next node = %p\n", currentNode->next->previous);
+			printf("current adress next = %p\n", currentNode->next);
+			printf("current adress next previous = %p\n", currentNode->next->previous);
 		}
-		printf("--------------------\n\n");
+		printf("\n");
+		printf("\n");
 
 		currentNode = currentNode->next;
 		nodeIndex++;
 	}
 }
 
-<<<<<<< HEAD:manageMemoryPart1.c
 // shouldn't have a main since we use #include "this file"
 /*
 void main() {
@@ -343,103 +312,6 @@ void main() {
 	//printContent();
 
 	struct node *nextNode = root->next;
-=======
-// Affiche_parametres_memoire
-void printMemoryParameters() {
-	printf("MEMORY PARAMETERS\n");
-	printf("Size of largest bloc of free memory : %d\n", sizeOfLargestBlocOfFreeMemory());
-	printf("Memory available : %d\n", availableMemory());
-	printf("Number of free blocs of memory : %d\n", nbFreeBlocs());
-	printf("Number of allocated blocs of memory : %d\n", nbAllocatedBlocs());
-	printf("Number of blocs of memory smaller than 100 : %d\n\n", nbFreeBlocsSmallerThan(100));
-}
-
-void firstFit() {
-	printf("START OF FIRST-FIT STRATEGY\n\n");
-	initMemory(500);
-	bloc* bloc0 = allocateMemory(100);
-	bloc* bloc1 = allocateMemory(100);
-	bloc* bloc2 = allocateMemory(100);
-	bloc* bloc3 = allocateMemory(100);
-	bloc* bloc4 = allocateMemory(100);
-	printMemoryState();
-	printMemoryParameters();
-
-	printf("FREEING MEMORY OF NODE 1\n\n");
-	freeMemory(bloc1);
-	printMemoryState();
-	printMemoryParameters();
-
-	printf("FREEING MEMORY OF NODE 3\n\n");
-	freeMemory(bloc3);
-	printMemoryState();
-	printMemoryParameters();
-
-	printf("FREEING MEMORY OF NODE 2\n\n");
-	freeMemory(bloc2);
-	printMemoryState();
-	printMemoryParameters();
-	printf("END OF FIRST-FIT STRATEGY\n");
-}
-
-void bestFit() {
-	printf("START OF BEST-FIT STRATEGY\n\n");
-	initMemory(600);
-	bloc* bloc0 = allocateMemory(100);
-	bloc* bloc1 = allocateMemory(100);
-	bloc* bloc2 = allocateMemory(100);
-	bloc* bloc3 = allocateMemory(100);
-	bloc* bloc4 = allocateMemory(100);
-	bloc* bloc5 = allocateMemory(100);
-	freeMemory(bloc2); // WHEN TRYING TO FREE bloc1 BEFORE bloc2,
-	freeMemory(bloc1); // SEGMENTATION FAULT
-	freeMemory(bloc4);
-	printMemoryState();
-	printMemoryParameters();
-
-	printf("END OF BEST-FIT STRATEGY\n");
-
-}
-
-void worstFit() {
-	printf("START OF WORST-FIT STRATEGY\n\n");
-	initMemory(500);
-	printMemoryState();
-	printMemoryParameters();
-	printf("END OF WORST-FIT STRATEGY\n");
-
-}
-
-void nextFit() {
-	printf("START OF NEXT-FIT STRATEGY\n\n");
-	initMemory(500);
-	printMemoryState();
-	printMemoryParameters();
-	printf("END OF NEXT-FIT STRATEGY\n");
-
-}
-
-void useAllocationStrategy(int strategyIndex) {
-	switch (strategyIndex) {
-		case 1:
-			bestFit();
-			break;
-		case 2:
-			worstFit();
-			break;
-		case 3:
-			nextFit();
-			break;
-		default:
-			firstFit();
-			break;
-	}
-}
-
-void main() {
-	useAllocationStrategy(0);
-}
->>>>>>> a5c1bf38455b9cab9622036f7eab1132e830025c:main.c
 
 	
 
